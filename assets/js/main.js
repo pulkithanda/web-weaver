30e5b663388528536e6801dd8d10a8163014b4b8
imsahiljain <emailofsahiljain@gmail.com>
2021-12-08 21:49:34 +0530

    add donation form

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,3 +38,22 @@ function scrollUp(){
     if(this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollUp)
+
+let donations = 0;
+let selected = false;
+let selId = 0;
+
+function setValue(element){
+  let val = element;
+  if(!selected){
+    val.style.background = "#3671f0";
+    val.style.color = "#fff";
+    selId = val;
+    selected = true;
+  }else{
+    selId.style.background = "#3671f0";
+    val.style.background = "#3671f0";
+    val.style.color = "#fff";
+    selId = val;
+  }
+}
